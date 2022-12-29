e7045aaa1df071edf3ff6eee4073a98a63dd3a16
Magno Belloni <bellonidesouza6@gmail.com>
2022-12-29 19:17:57 -0300

    Add project files.

---
+++ BookingService/Core/Domain/Enums/Action.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enums
+{
+    public enum Action
+    {
+        Pay = 0,
+        Finish = 1,  // After paid and used
+        Cancel = 2,  // Can never be paid
+        Refound = 3, // Paid then refound
+        Reopeon = 4, // Canceled
+    }
+}
